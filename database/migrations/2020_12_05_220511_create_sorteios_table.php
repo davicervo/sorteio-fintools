d3e74673fdb57072eeb73851f73e94b2b8f2b2a4
João de Souza <joaogbdesouza@gmail.com>
2020-12-08 20:55:09 -0300

    migration de sorteio

--- database/migrations/2020_12_05_220511_create_sorteios_table.php
+++ database/migrations/2020_12_05_220511_create_sorteios_table.php
@@ -11,6 +11,29 @@ class CreateSorteiosTable extends Migration
     {
         Schema::create('sorteios', function (Blueprint $table) {
             $table->uuid('sorteio_uid')->primary();
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->date('data_sorteio');
+            $table->boolean('ativo');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timeStamps();
+            $table->softDeletes();
+            $table->integer('deleted_by');
+            
+            // FK's
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->foreign('deleted_by')->references('id')->on('users');
+            
+            // Indexs
+            $table->index('titulo');
+            $table->index('data_sorteio');
+            $table->index('ativo');
+            $table->index('created_by');
+            $table->index('updated_by');
+            $table->index('deleted_by');
         });
     }
 
