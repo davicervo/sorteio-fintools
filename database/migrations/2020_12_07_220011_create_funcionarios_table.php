f7c4e013a77a2a91f3d5e994585308f8f7831c62
luccazx <Poterzil21>
2020-12-07 23:58:52 -0300

    alteracao de campos obrigatorios de funcionarios

--- database/migrations/2020_12_07_220011_create_funcionarios_table.php
+++ database/migrations/2020_12_07_220011_create_funcionarios_table.php
@@ -12,15 +12,13 @@ class CreateFuncionariosTable extends Migration
         Schema::create('funcionarios', function (Blueprint $table) {
             $table->uuid('funcionario_uid')->primary();
             $table->string('nome', 60);
-            $table->text('foto');
-            $table->foreignUuid('funcao_uid')->constrained('funcoes', 'funcao_uid');
+            $table->text('foto')->nullable();
+            //$table->foreignUuid('funcao_uid')->constrained('funcoes', 'funcao_uid');
             $table->foreignUuid('departamento_uid')->constrained('departamentos', 'departamento_uid');
             $table->boolean('elegivel')->default(false);
             $table->string('created_by', 100)->nullable()->default(null);
-            $table->timestamp('created_at', 0)->nullable();
             $table->string('updated_by', 100)->nullable()->default(null);
-            $table->timestamp('updated_at', 0)->nullable();
-            $table->string('deleted_by', 100)->nullable()->default(null);
+            $table->timestamps();
             $table->softDeletes();
         });
     }
