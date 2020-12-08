b25844bffe076a09d836a4b9f6824ffed03c2e5d
luccazx <Poterzil21>
2020-12-07 23:54:44 -0300

    rota api de funcionarios

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => ['auth'],'prefix' => 'funcionarios'], function () {
+    Route::get('', 'FuncionarioController@index');
+    Route::get('{uid}', 'FuncionarioController@find');
+    Route::post('', 'FuncionarioController@find@store');
+    Route::put('{uid}', 'FuncionarioController@update');
 });
