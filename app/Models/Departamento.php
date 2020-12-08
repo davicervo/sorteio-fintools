7807a30029168d1ea3f935a563aa10f91d91041f
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2020-12-07 21:56:11 -0300

    Criadas Models e Controllers para departamento, funcionario e funcao

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use App\Models\Funcionario;
 use Illuminate\Database\Eloquent\Model;
 
 class Departamento extends Model
 {
-    //
+    use UuidTrait;
+
+    public $incrementing = false;
+    protected $primaryKey = 'departamento_uid';
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nome_exibicao',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function funcionarios()
+    {
+        return $this->hasMany(Funcionario::class, 'funcionario_uid', 'funcionario_uid');
+    }
 }
