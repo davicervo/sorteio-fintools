74e76aa06237e105b44ab7274731e9f532488814
João de Souza <joaogbdesouza@gmail.com>
2020-12-08 21:26:44 -0300

    Sorteios Seeder

---
+++ database/seeds/SorteiosSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Sorteio;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+
+class SorteiosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 100; $i++){
+            $sorteio = [
+                'titulo' => $faker->sentence(4, true),
+                'descricao' => $faker->text(200),
+                'data_sorteio' => $faker->date(),
+                'ativo' => $faker->boolean(),
+                'created_by' => 1,
+                'updated_by' => null,
+                'deleted_by' => null
+            ];
+
+            Sorteio::create($sorteio);
+        }
+    }
+}
