c31f60f033a9a8b9985b00a4ac199aef3b0b4ec2
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2020-12-11 15:46:44 -0300

    Corrigindo falha na rota do sorteio

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::view('/{uid}', 'sorteio')->name('sorteio');
+    Route::view('sorteio/{uid}', 'sorteio')->name('sorteio');
 
     Route::prefix('brindes')->group(function () {
         Route::get('', 'BrindeController@index')->name('brindes.index');
