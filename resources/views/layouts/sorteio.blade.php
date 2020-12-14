5dd6e1816460add448dcca3084c8d991b58b7d32
Daniel Guimarães <danielazevedo2k12@gmail.com>
2020-12-14 11:38:58 -0300

    editando botao

--- resources/views/layouts/sorteio.blade.php
+++ resources/views/layouts/sorteio.blade.php
@@ -27,6 +27,21 @@
             background-attachment: fixed;
             height: 100vh;
         }
+        .sombra{
+            -webkit-box-shadow: -1px -4px 24px 0 rgba(255,128,128,0.75);
+            -moz-box-shadow: -1px -4px 24px 0 rgba(255,128,128,0.75);
+            box-shadow: -1px -4px 24px 0 rgba(255,128,128,0.75);
+            border: none;
+            position: fixed; top: 10px; right: 20px;
+        }
+        .sombra span{border: none;}
+        .sombra .icon{background: #ad0000;margin-right: -10px;}
+        .sombra .text{background: #e60000;}
+
+        .sombra:hover .icon i{
+            transition: transform 500ms;
+            transform: translateX(-5px) scale(1.5);
+        }
 
         html,
         body {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.sorteio')
 
 @section('content')
-<a href="{{route('home')}}" class="btn btn-lg" style="background: #ad0000; position: fixed; top: 10px; right: 10px; color: white;">
-    <i class="fa fa-cogs"></i>&nbsp;Painel de Controle
-</a>
+{{--<a href="{{route('home')}}" class="btn btn-lg" style="background: #ad0000; position: fixed; top: 10px; right: 10px; color: white;">--}}
+{{--    <i class="fa fa-cogs"></i>&nbsp;Painel de Controle--}}
+{{--</a>--}}
+    <div class="text-center sombra mt-3">
+        <span class="btn btn-light btn-lg text-white icon">
+            <i class="fa fa-cogs"></i>
+        </span>
+        <span class="btn btn-light btn-lg text-white text">
+            Ir para o Sorteio
+        </span>
+    </div>
 <div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
         <div class="col-12">
